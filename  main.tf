89dcb48705531ac15c3309795d7a2bdc27ddc49d
felipe cazuza <felipedias1990@gmail.com>
2023-11-06 23:19:37 -0300

    terraform-ansible-python

---  main.tf
+++  main.tf
@@ -32,15 +32,24 @@ resource "aws_security_group" "app_security_group"{
 
 # Regras de entrada permitem o tráfego necessário para a aplicação
   ingress{
-    from_port         = 80
-    to_port           = 80
+    from_port         =  0
+    to_port           = 65535
+    protocol          = "tcp"
+    cidr_blocks        = ["0.0.0.0/0"]
+  }
+
+  # Regras de saida permitem o tráfego necessário para a aplicação
+   ingress{
+    from_port         =  0
+    to_port           = 65535
     protocol          = "tcp"
     cidr_blocks        = ["0.0.0.0/0"]
   }
 }
 
+
 resource "aws_subnet" "app_subnet" {
-  count               = 3
+  count               = 1
   vpc_id              = aws_vpc.teste.id
   cidr_block          = "10.0.${count.index}.0/24" 
   availability_zone   = "us-east-1a"
@@ -53,14 +62,14 @@ resource "aws_subnet" "app_subnet" {
 
 resource "aws_instance" "app_server" {
   count         = 1
-  ami           = "ami-041feb57c611358bd"
+  ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro" 
   key_name = "tst"
   subnet_id = aws_subnet.app_subnet[0].id  
   security_groups =[aws_security_group.app_security_group.id]
   
   tags = {
-    Name = "Server.terraform"
+    Name = "Terraform Ansible Python"
   }
 }
 
